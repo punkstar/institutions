7bbf6a0c1ce63b886ee7318c54180ba0d5924bcc
Nick Jones <nick@nicksays.co.uk>
2010-12-31 02:41:10 +0000

    Base institution service.

---
+++ institution/src/main/java/uk/ac/bath/cs/agents/asinst/InstitutionService.java
@@ -0,0 +1,8 @@
+package uk.ac.bath.cs.agents.asinst;
+
+import org.iids.aos.service.Service;
+import org.iids.aos.service.ServiceListener;
+
+public interface InstitutionService extends Service {
+    
+}
---
+++ institution/src/main/java/uk/ac/bath/cs/agents/asinst/InstitutionServiceImpl.java
@@ -0,0 +1,9 @@
+package uk.ac.bath.cs.agents.asinst;
+
+import org.iids.aos.service.AbstractDefaultService;
+
+import uk.ac.bath.cs.agents.asinst.InstitutionService;
+
+public class InstitutionServiceImpl extends AbstractDefaultService implements InstitutionService {
+    
+}
