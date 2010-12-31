d47fc20c402ff6c0e6ae2114bf691e460a6b9051
Nick Jones <nick@nicksays.co.uk>
2010-12-31 03:42:30 +0000

    Sort out the service files.

--- institution.shared/src/main/java/uk/ac/bath/cs/agents/asinst/InstitutionFactoryService.java
+++ institution.shared/src/main/java/uk/ac/bath/cs/agents/asinst/InstitutionFactoryService.java
@@ -3,6 +3,6 @@ package uk.ac.bath.cs.agents.asinst;
 import org.iids.aos.service.Service;
 import org.iids.aos.service.ServiceListener;
 
-public interface InstitutionService extends Service {
+public interface InstitutionFactoryService extends Service {
     
 }
