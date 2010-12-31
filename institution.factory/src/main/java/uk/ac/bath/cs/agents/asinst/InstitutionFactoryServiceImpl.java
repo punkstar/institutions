0d37f5117d4b916f09643bfa2d3655fdcb31646e
Nick Jones <nick@nicksays.co.uk>
2010-12-31 02:35:50 +0000

    Base institution factory.

---
+++ institution.factory/src/main/java/uk/ac/bath/cs/agents/asinst/InstitutionFactoryServiceImpl.java
@@ -0,0 +1,9 @@
+package uk.ac.bath.cs.agents.asinst;
+
+import org.iids.aos.service.AbstractDefaultService;
+
+import uk.ac.bath.cs.agents.asinst.InstitutionFactoryService;
+
+public class InstitutionFactoryServiceImpl extends AbstractDefaultService implements InstitutionFactoryService {
+    
+}
